8ef713089f7cccb4623070b7ddf8e2967b04700a
msandmann <maximilian@vondelog.com>
2017-11-23 04:04:41 -0500

    game logic unit 11

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,6 +21,7 @@ var cards = [
   },
 ];
 
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -34,20 +35,27 @@ var checkForMatch = function() {
 
 }
 
-var flipCard = function (cardId){
-  console.log("User flipped " + cards[cardId].rank);
-  console.log(cards[cardId].cardImage);
-  console.log(cards[cardId].suit);
-  cardsInPlay.push(cards[cardId].rank);
+var flipCard = function (){
+  console.log("User flipped " + cards[this.getAttribute('data-id')].rank);
+  console.log(cards[this.getAttribute('data-id')].cardImage);
+  console.log(cards[this.getAttribute('data-id')].suit);
+  cardsInPlay.push(cards[this.getAttribute('data-id')].rank);
+
+  this.setAttribute('src', cards[this.getAttribute('data-id')].cardImage);
 
   if (cardsInPlay.length === 2){
     checkForMatch();
   }
-
-
 }
 
+var createBoard = function(){
+  for (var i = 0; i < cards.length; i++){
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', "images/back.png");
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
 
-
-flipCard(0);
-flipCard(2);
+createBoard();
